5d9afbf05f5704ef08bef142aec5aef21c74a7ec
hugh-bowie <hughbowie@me.com>
2021-08-11 13:30:42 -0400

    code Snapshot 22-3

---
+++ client/build/precache-manifest.e7833423b81a2fbce53dfb3b1995d557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f8cf0ef69c152ef54094dc2e9b99ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd37a0901c815bc87594",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "c7a0eaa0024f1d705286",
+    "url": "/static/js/2.f1436fb1.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.f1436fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd37a0901c815bc87594",
+    "url": "/static/js/main.2a68b6e1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
